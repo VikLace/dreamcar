62a751e3a9f2bdfb41773d97e81f590c893e8bb8
Viktorija Gončarova <v.goncarova.work@gmail.com>
2021-03-05 12:38:48 +0200

    added car interface

---
+++ src/app/interfaces/ICar.ts
@@ -0,0 +1,35 @@
+export interface ICar{
+  brand: string;
+  manufacturer: string;
+  name: string;
+  productionDate: number; //year
+  vehicleConfiguration: string;
+
+  image: string;
+  outerLink: string;
+  description: string;
+
+  bodyType: string;
+  height: number; //centimeters
+  width: number; //centimeters
+  wheelbase: number; //centimeters
+  weight: number; //kg
+  numberOfDoors: number;
+  seatingCapacity: number;
+  cargoVolume: number; //litres
+
+  vehicleTransmission: string;
+  numberOfForwardGears: number;
+  accelerationTime: number; //seconds
+  speed: number; //hm/h
+  engineDisplacement: number; //qubic centimeters
+  enginePower: number; //HP
+  torque: number; //Nm
+
+  fuelType: string;
+  fuelCapacity: number; //litres
+  fuelConsumption: number; //litres/100km
+
+  emissionsCO2: number; //g/km
+  meetsEmissionStandard: string;
+}
