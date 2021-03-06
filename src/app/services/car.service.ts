097a6fa6d48f07f1fa71e57bd329f3ecde8c93ba
Viktorija Gončarova <v.goncarova.work@gmail.com>
2021-03-06 13:15:41 +0200

    added car service to utilise mock data

---
+++ src/app/services/car.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Car } from 'app/interfaces/car.intf';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  private carsUrl = 'api/cars';
+
+  constructor(private http: HttpClient) { }
+
+  getCars(): Observable<Car[]> {
+    return this.http.get<Car[]>(this.carsUrl);
+  }
+}
