ed4036fa3cb4023a300a432abdec248d7915f3b9
Viktorija Gončarova <v.goncarova.work@gmail.com>
2021-03-07 11:46:08 +0200

    added app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarsComponent } from 'app/cars/cars.component';
+import { CarDetesComponent } from 'app/car-detes/car-detes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/cars', pathMatch: 'full' },
+  { path: 'cars', component: CarsComponent },
+  { path: 'cars/:id', component: CarDetesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
