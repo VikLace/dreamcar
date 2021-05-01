feb68cef6ac30f47c4d70138e136574d1537aca8
Viktorija Gončarova <v.goncarova.work@gmail.com>
2021-05-01 17:42:10 +0300

    removed app routes, now it is SPA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { CarsComponent } from 'app/cars/cars.component';
 import { CarDetesComponent } from 'app/car-detes/car-detes.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/cars', pathMatch: 'full' },
-  { path: 'cars', component: CarsComponent },
-  { path: 'cars/:id', component: CarDetesComponent },
-];
+const routes: Routes = [{ path: '', component: CarsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
